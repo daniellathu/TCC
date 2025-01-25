520afc28e56767b29bae60215fbed8c48afa47cf
daniellathu <158127553+daniellathu@users.noreply.github.com>
2025-01-25 11:59:15 +0800

    Update add.php

--- add.php
+++ add.php
@@ -6,10 +6,10 @@
 <body>
 	<h2>Add Data</h2>
 	<p>
-		<a href="/TCC/index.php">Home</a>
+		<a href="/index.php">Home</a>
 	</p>
 
-	<form action="/TCC/addAction.php" method="post" name="add">
+	<form action="/addAction.php" method="post" name="add">
 		<table width="25%" border="0">
 			<tr> 
 				<td>Name</td>
